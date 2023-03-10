7e9ff009e0b744476fcc1876f5bf0d5d31d10399
Akhil Reddy <akhilkaluva@gmail.com>
2023-03-10 14:10:51 +0000

    Reusable Banners

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const BannerCardItem = props => {
+  const {bannerCard} = props
+  const {headerText, description, className} = bannerCard
+  return (
+    <li className={`card ${className}`}>
+      <div>
+        <h1 className="heading">{headerText}</h1>
+        <p className="para">{description}</p>
+        <button className="button">Show More</button>
+      </div>
+    </li>
+  )
+}
+export default BannerCardItem
